bfff2978fbc35c0ba1ee061c78a79e4400f04013
Polashahmad01 <dev.polashahmad@gmail.com>
2025-09-14 22:25:02 +0600

    Add styles for privacy policy page

--- src/pages/PrivacyPolicyPage.tsx
+++ src/pages/PrivacyPolicyPage.tsx
@@ -5,10 +5,10 @@ export default function PrivacyPolicyPage() {
         Privacy Policy
       </h1>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
-        At TattooDesignPro, accessible from https://tattoodesignpro.com, one of
+        At FrontEnd Starter, accessible from https://frontendstarter.com, one of
         our main priorities is the privacy of our visitors. This Privacy Policy
         document contains types of information that is collected and recorded by
-        TattooDesignPro and how we use it.
+        FrontEnd Starter and how we use it.
       </p>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         If you have additional questions or require more information about our
@@ -17,7 +17,7 @@ export default function PrivacyPolicyPage() {
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         This Privacy Policy applies only to our online activities and is valid
         for visitors to our website with regards to the information that they
-        shared and/or collect in TattooDesignPro. This policy is not applicable
+        shared and/or collect in FrontEnd Starter. This policy is not applicable
         to any information collected offline or via channels other than this
         website.
       </p>
@@ -28,7 +28,9 @@ export default function PrivacyPolicyPage() {
         to its terms.
       </p>
 
-      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">Information we collect</h2>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Information we collect
+      </h2>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         The personal information that you are asked to provide, and the reasons
         why you are asked to provide it, will be made clear to you at the point
@@ -46,13 +48,25 @@ export default function PrivacyPolicyPage() {
         address, and telephone number.
       </p>
 
-      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">How we use your information</h2>
-      <p className="text-sm font-light leading-6 opacity-80 lg:w-10/12">We use the information we collect in various ways, including to:</p>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        How we use your information
+      </h2>
+      <p className="text-sm font-light leading-6 opacity-80 lg:w-10/12">
+        We use the information we collect in various ways, including to:
+      </p>
       <ul className="list-disc text-sm font-light mb-4 lg:w-10/12">
-        <li className="ml-8 py-1 italic">Provide, operate, and maintain our website.</li>
-        <li className="ml-8 py-1 italic">Improve, personalize, and expand our website.</li>
-        <li className="ml-8 py-1 italic">Understand and analyze how you use our website.</li>
-        <li className="ml-8 py-1 italic">Develop new products, services, features, and functionality.</li>
+        <li className="ml-8 py-1 italic">
+          Provide, operate, and maintain our website.
+        </li>
+        <li className="ml-8 py-1 italic">
+          Improve, personalize, and expand our website.
+        </li>
+        <li className="ml-8 py-1 italic">
+          Understand and analyze how you use our website.
+        </li>
+        <li className="ml-8 py-1 italic">
+          Develop new products, services, features, and functionality.
+        </li>
         <li className="ml-8 py-1 italic">
           Communicate with you, either directly or through one of our partners,
           including for customer service, to provide you with updates and other
@@ -63,9 +77,11 @@ export default function PrivacyPolicyPage() {
         <li className="ml-8 py-1 italic">Find and prevent fraud.</li>
       </ul>
 
-      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">Log Files</h2>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Log Files
+      </h2>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
-        TattooDesignPro follows a standard procedure of using log files. These
+        FrontEnd Starter follows a standard procedure of using log files. These
         files log visitors when they visit websites. All hosting companies do
         this and a part of hosting services analytics. The information collected
         by log files include internet protocol (IP) addresses, browser type,
@@ -76,9 +92,11 @@ export default function PrivacyPolicyPage() {
         users movement on the website, and gathering demographic information.
       </p>
 
-      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">Cookies and Web Beacons</h2>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Cookies and Web Beacons
+      </h2>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
-        Like any other website, TattooDesignPro uses &quot;cookies&quot;. These
+        Like any other website, FrontEnd Starter uses &quot;cookies&quot;. These
         cookies are used to store information including visitors preferences,
         and the pages on the website that the visitor accessed or visited. The
         information is used to optimize the users experience by customizing our
@@ -86,128 +104,151 @@ export default function PrivacyPolicyPage() {
         information.
       </p>
 
-      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">Advertising Partners Privacy Policiess</h2>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Advertising Partners Privacy Policiess
+      </h2>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         You may consult this list to find the Privacy Policy for each of the
-        advertising partners of TattooDesignPro.
+        advertising partners of FrontEnd Starter.
       </p>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         Third-party ad servers or ad networks uses technologies like cookies,
         JavaScript, or Web Beacons that are used in their respective
-        advertisements and links that appear on TattooDesignPro, which are sent
+        advertisements and links that appear on FrontEnd Starter, which are sent
         directly to users browser. They automatically receive your IP address
         when this occurs. These technologies are used to measure the
         effectiveness of their advertising campaigns and/or to personalize the
         advertising content that you see on websites that you visit.
       </p>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
-        Note that TattooDesignPro has no access to or control over these cookies
-        that are used by third-party advertisers.
+        Note that FrontEnd Starter has no access to or control over these
+        cookies that are used by third-party advertisers.
       </p>
 
-      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">Third Party Privacy Policies</h2>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Third Party Privacy Policies
+      </h2>
       <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
-        TattooDesignPro&apos;s Privacy Policy does not apply to other
+        FrontEnd Starter&apos;s Privacy Policy does not apply to other
         advertisers or websites. Thus, we are advising you to consult the
         respective Privacy Policies of these third-party ad servers for more
         detailed information. It may include their practices and instructions
         about how to opt-out of certain options.
       </p>
-      <p>
+      <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         You can choose to disable cookies through your individual browser
         options. To know more detailed information about cookie management with
         specific web browsers, it can be found at the browsers respective
         websites.
       </p>
 
-      <h2>CCPA Privacy Rights (Do Not Sell My Personal Information)</h2>
-      <p>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        CCPA Privacy Rights (Do Not Sell My Personal Information)
+      </h2>
+      <p className="text-sm font-light leading-6 opacity-80 lg:w-10/12">
         Under the CCPA, among other rights, California consumers have the right
         to:
       </p>
-      <p>
-        Request that a business that collects a consumer&apos;s personal data
-        disclose the categories and specific pieces of personal data that a
-        business has collected about consumers.
-      </p>
-      <p>
-        Request that a business delete any personal data about the consumer that
-        a business has collected.
-      </p>
-      <p>
-        Request that a business that sells a consumer&apos;s personal data, not
-        sell the consumer&apos;s personal data.
-      </p>
-      <p>
-        If you make a request, we have one month to respond to you. If you would
-        like to exercise any of these rights, please contact us.
-      </p>
+      <ul className="list-disc text-sm font-light mb-4 lg:w-10/12">
+        <li className="ml-8 py-1 italic">
+          Request that a business that collects a consumer&apos;s personal data
+          disclose the categories and specific pieces of personal data that a
+          business has collected about consumers.
+        </li>
+        <li className="ml-8 py-1 italic">
+          Request that a business delete any personal data about the consumer
+          that a business has collected.
+        </li>
+        <li className="ml-8 py-1 italic">
+          Request that a business that sells a consumer&apos;s personal data,
+          not sell the consumer&apos;s personal data.
+        </li>
+        <li className="ml-8 py-1 italic">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </li>
+      </ul>
 
-      <h2>GDPR Data Protection Rights</h2>
-      <p>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        GDPR Data Protection Rights
+      </h2>
+      <p className="text-sm font-light leading-6 opacity-80 lg:w-10/12">
         We would like to make sure you are fully aware of all of your data
         protection rights. Every user is entitled to the following:
       </p>
-      <p>
-        The right to access – You have the right to request copies of your
-        personal data. We may charge you a small fee for this service.
-      </p>
-      <p>
-        The right to rectification – You have the right to request that we
-        correct any information you believe is inaccurate. You also have the
-        right to request that we complete the information you believe is
-        incomplete.
-      </p>
-      <p>
-        The right to erasure – You have the right to request that we erase your
-        personal data, under certain conditions.
-      </p>
-      <p>
-        The right to restrict processing – You have the right to request that we
-        restrict the processing of your personal data, under certain conditions.
-      </p>
-      <p>
-        The right to object to processing – You have the right to object to our
-        processing of your personal data, under certain conditions.
-      </p>
-      <p>
-        The right to data portability – You have the right to request that we
-        transfer the data that we have collected to another organization, or
-        directly to you, under certain conditions.
-      </p>
-      <p>
-        If you make a request, we have one month to respond to you. If you would
-        like to exercise any of these rights, please contact us.
-      </p>
+      <ul className="list-disc text-sm font-light mb-4 lg:w-10/12">
+        <li className="ml-8 py-1 italic">
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+        </li>
+        <li className="ml-8 py-1 italic">
+          The right to access – You have the right to request copies of your
+          personal data. We may charge you a small fee for this service.
+        </li>
+        <li className="ml-8 py-1 italic">
+          The right to rectification – You have the right to request that we
+          correct any information you believe is inaccurate. You also have the
+          right to request that we complete the information you believe is
+          incomplete.
+        </li>
+        <li className="ml-8 py-1 italic">
+          The right to erasure – You have the right to request that we erase
+          your personal data, under certain conditions.
+        </li>
+        <li className="ml-8 py-1 italic">
+          The right to restrict processing – You have the right to request that
+          we restrict the processing of your personal data, under certain
+          conditions.
+        </li>
+        <li className="ml-8 py-1 italic">
+          The right to object to processing – You have the right to object to
+          our processing of your personal data, under certain conditions.
+        </li>
+        <li className="ml-8 py-1 italic">
+          The right to data portability – You have the right to request that we
+          transfer the data that we have collected to another organization, or
+          directly to you, under certain conditions.
+        </li>
+        <li className="ml-8 py-1 italic">
+          If you make a request, we have one month to respond to you. If you
+          would like to exercise any of these rights, please contact us.
+        </li>
+      </ul>
 
-      <h2>Children&apos;s Information</h2>
-      <p>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Children&apos;s Information
+      </h2>
+      <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         Another part of our priority is adding protection for children while
         using the internet. We encourage parents and guardians to observe,
         participate in, and/or monitor and guide their online activity.
       </p>
-      <p>
-        TattooDesignPro does not knowingly collect any Personal Identifiable
+      <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
+        FrontEnd Starter does not knowingly collect any Personal Identifiable
         Information from children under the age of 13. If you think that your
         child provided this kind of information on our website, we strongly
         encourage you to contact us immediately and we will do our best efforts
         to promptly remove such information from our records.
       </p>
 
-      <h2>Changes to This Privacy Policy</h2>
-      <p>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Changes to This Privacy Policy
+      </h2>
+      <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         We may update our Privacy Policy from time to time. Thus, we advise you
         to review this page periodically for any changes. We will notify you of
         any changes by posting the new Privacy Policy on this page. These
         changes are effective immediately, after they are posted on this page.
       </p>
-      <p>
+      <p className="text-sm font-light leading-6 opacity-80 mb-4 lg:w-10/12">
         Our Privacy Policy was created with the help of the Privacy Policy
         Generator.
       </p>
 
-      <h2>Contact Us</h2>
-      <p>
+      <h2 className="text-xl font-bold mb-4 opacity-80 lg:w-10/12">
+        Contact Us
+      </h2>
+      <p className="text-sm font-light leading-6 opacity-80 mb-4 pb-6 lg:w-10/12">
         If you have any questions or suggestions about our Privacy Policy, do
         not hesitate to contact us.
       </p>
