44e1c982f34428c5506f1442ee1f0f5e25f349ba
Polashahmad01 <dev.polashahmad@gmail.com>
2025-09-16 01:58:57 +0600

    add join page

--- src/pages/JoinPage.tsx
+++ src/pages/JoinPage.tsx
@@ -1,68 +1,84 @@
 import { Link } from "react-router";
-import { FaGoogle, FaApple } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
+import { FaGoogle, FaApple, FaEnvelope } from "react-icons/fa";
 import { GoZap } from "react-icons/go";
 import MarqueeEffect from "../components/MarqueeEffect";
 
 export default function JoinPage() {
   return (
-    <section className="grid place-items-center h-full md:grid-cols-2">
-      <div className="hidden md:block">
-        <div className="bg-[#f3f3f3] rounded-3xl flex items-center justify-center px-4 py-4 overflow-hidden md:h-96 md:w-90 xl:w-[450px]">
+    <section className="flex justify-center items-center h-screen py-6 sm:py-0">
+      <article className="hidden md:flex md:justify-center md:items-center md:flex-1 bg-[#f3f3f3] h-full">
+        {/* <div className="bg-[#f3f3f3] rounded-3xl flex items-center justify-center px-4 py-4 overflow-hidden md:h-96 md:w-90 xl:w-[450px]">
+          <MarqueeEffect />
+        </div> */}
+        <div className="px-6">
           <MarqueeEffect />
         </div>
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <div className="w-20 h-20 mb-10 flex justify-center items-center rounded-2xl border border-[rgba(0,0,0,0.2)] bg-[rgba(0,0,0,0.2)] animate-pulse delay-100">
-          <GoZap size={40} className="animate-pulse" />
-        </div>
-        <div className="text-center mb-4 lg:flex lg:justify-center">
-          <h1 className="text-xl font-bold leading-none tracking-normal opacity-80 md:text-3xl lg:text-4xl lg:max-w-5/6 xl:text-5xl 2xl:text-6xl">
-            Join millions of Happy Users
-          </h1>
-        </div>
-        <div className="text-center mb-4">
-          <p className="text-base leading-normal tracking-normal max-w-75">
-            Your data is safe with us, and you’ll never receive spam.
-          </p>
-        </div>
-        <div className="text-center mb-4">
-          <p className="text-base leading-tight tracking-normal">
-            Continue with one of these:
-          </p>
-        </div>
-        <div className="flex flex-col gap-3 mb-4">
-          <button className="w-full cursor-pointer rounded-full px-6 py-2 flex items-center gap-4 bg-[#f3f3f3] hover:bg-[#fafafa]">
-            <FaGoogle size={15} />
-            <span>Sign in with Google</span>
-          </button>
-          <button className="w-full cursor-pointer rounded-full px-6 py-2 flex items-center gap-4 bg-[#f3f3f3] hover:bg-[#fafafa]">
-            <FaApple size={20} />
-            <span>Sign in with Apple</span>
-          </button>
-          <Link to="/sign-up" className="w-full cursor-pointer rounded-full px-6 py-2 flex items-center gap-4 bg-[#f3f3f3] hover:bg-[#fafafa]">
-            <MdEmail size={17} />
-            <span>Continue with Email</span>
-          </Link>
-        </div>
-        <div className="text-center">
-          <p className="text-xs font-extralight leading-normal tracking-normal text-[rgba(135,135,135,1)]">
-            By continuing, you agree to FrontEnd Starter’s
-            <br />
-            <Link to="/terms-of-use" className="underline text-[rgba(0,0,0,1)]">
-              Terms of Use.
-            </Link>{" "}
-            Read our{" "}
+      </article>
+      <article className="flex justify-center items-center flex-1 h-full">
+        <div className="flex flex-col items-center w-5/6">
+          <div className="w-20 h-20 flex justify-center items-center mb-8 rounded-2xl border border-[rgba(0,0,0,0.2)] bg-[rgba(0,0,0,0.2)] animate-pulse delay-100">
+            <GoZap size={40} className="animate-pulse" />
+          </div>
+          <div className="text-center mb-4 lg:flex lg:justify-center">
+            <h1 className="text-xl font-bold leading-none tracking-normal opacity-80 md:text-3xl lg:text-4xl lg:max-w-5/6 xl:text-5xl 2xl:text-6xl">
+              Join millions of Happy Users
+            </h1>
+          </div>
+          <div className="text-center mb-2">
+            <p className="text-base leading-normal tracking-normal max-w-75">
+              Your data is safe with us, and you’ll never receive spam.
+            </p>
+          </div>
+          <div className="text-center mb-6">
+            <p className="text-base leading-tight tracking-normal">
+              Continue with one of these:
+            </p>
+          </div>
+          <div className="w-full h-px bg-gradient-to-r from-transparent via-cyan-400 to-transparent mb-6" />
+          <div className="flex flex-col gap-3 mb-6 xl:w-1/2">
+            <button className="w-full cursor-pointer rounded-full px-6 py-2 flex items-center justify-center bg-[#f3f3f3] hover:bg-[#fafafa]">
+              <div className="flex items-center gap-4">
+                <FaGoogle size={15} />
+                <span>Sign in with Google</span>
+              </div>
+            </button>
+            <button className="w-full cursor-pointer rounded-full px-6 py-2 flex items-center justify-center bg-[#f3f3f3] hover:bg-[#fafafa]">
+              <div className="flex items-center gap-4">
+                <FaApple size={20} />
+                <span>Sign in with Apple</span>
+              </div>
+            </button>
             <Link
-              to="/privacy-policy"
-              className="underline text-[rgba(0,0,0,1)]"
+              to="/sign-up"
+              className="w-full cursor-pointer rounded-full px-6 py-2 flex items-center justify-center bg-[#f3f3f3] hover:bg-[#fafafa]"
             >
-              Privacy Policy.
+              <div className="flex items-center gap-4">
+                <FaEnvelope size={16} />
+                <span>Continue with Email</span>
+              </div>
             </Link>
-          </p>
+          </div>
+          <div className="text-center">
+            <p className="text-xs font-extralight leading-normal tracking-normal text-[rgba(135,135,135,1)]">
+              By continuing, you agree to FrontEnd Starter’s
+              <br />
+              <Link
+                to="/terms-of-use"
+                className="underline text-[rgba(0,0,0,1)]"
+              >
+                Terms of Use.
+              </Link>{" "}
+              Read our{" "}
+              <Link
+                to="/privacy-policy"
+                className="underline text-[rgba(0,0,0,1)]"
+              >
+                Privacy Policy.
+              </Link>
+            </p>
+          </div>
         </div>
-      </div>
+      </article>
     </section>
   );
 }
